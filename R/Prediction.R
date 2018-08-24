b9af339f89dd3c7e6b64fdf829bba6c64d863f51
Richard <richard.redding@starstone.com>
2018-08-24 08:46:22 +0100

    oops, forgot to rename some arguments in prediction

--- R/Prediction.R
+++ R/Prediction.R
@@ -1,4 +1,4 @@
 predict_numFutureEvents <- function(staticTerm, observedTerm, predictionTerm, v_r_paymentRate, v_r_settleRate){
-  fn_imputeData_expected(inactiveTerm, observedTerm, v_r_paymentRate, v_r_settleRate)$censored *
+  fn_imputeData_expected(staticTerm, observedTerm, v_r_paymentRate, v_r_settleRate)$censored *
     fn_truncatedExponential_exp(v_r_settleRate, 0, predictionTerm) * v_r_paymentRate
 }
