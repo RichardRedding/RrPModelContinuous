65f56bdce4ea079c5461b3382db6799510f0153d
Richard <richard.redding@starstone.com>
2018-08-25 17:09:05 +0100

    changed argument names to match parameter estimation naming

--- R/Prediction.R
+++ R/Prediction.R
@@ -1,4 +1,4 @@
-predict_numFutureEvents <- function(staticTerm, observedTerm, predictionTerm, v_r_paymentRate, v_r_settleRate){
-  fn_imputeData_expected(staticTerm, observedTerm, v_r_paymentRate, v_r_settleRate)$censored *
-    fn_truncatedExponential_exp(v_r_settleRate, 0, predictionTerm) * v_r_paymentRate
+predict_numFutureEvents <- function(staticTerm, observedTerm, predictionTerm, p, s){
+  fn_imputeData_expected(staticTerm, observedTerm, p, s)$censored *
+    fn_truncatedExponential_exp(s, 0, predictionTerm) * p
 }
